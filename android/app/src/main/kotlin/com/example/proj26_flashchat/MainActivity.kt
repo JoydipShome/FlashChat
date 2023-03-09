280d094d0b044d08e8d7a18148fbd6fba4579742
jshome <theshome@gmail.com>
2023-03-09 15:13:03 +0530

    First Commit
    This is flutter project - FlashChat

---
+++ android/app/src/main/kotlin/com/example/proj26_flashchat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.proj26_flashchat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import '../screens/welcome_screen.dart';
+import '../screens/login_screen.dart';
+import '../screens/registration_screen.dart';
+import '../screens/chat_screen.dart';
+
+// void main() => runApp(const FlashChat());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // theme: ThemeData.dark().copyWith(
+      //   textTheme: const TextTheme(
+      //     bodyText1: TextStyle(color: Colors.black54),
+      //   ),
+      // ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => const WelcomeScreen(),
+        LoginScreen.id: (context) => const LoginScreen(),
+        RegistrationScreen.id: (context) => const RegistrationScreen(),
+        ChatScreen.id: (context) => const ChatScreen(),
+      },
+    );
+  }
+}
